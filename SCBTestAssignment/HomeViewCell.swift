1aecc94786e2bbe16da44e45cdbbc76787189cd0
Hathai06 <49894005+Hathai06@users.noreply.github.com>
2019-12-29 23:22:17 +0700

    Add files via upload

---
+++ SCBTestAssignment/HomeViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewCell.swift
+//  SCBTestAssignment
+//
+//  Created by Nuan on 29/12/2562 BE.
+//  Copyright © 2562 Hathairat. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewCell()
+    }
+}
---
+++ SCBTestAssignment/SCBTestAssignment/MyTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MyTableViewCell.swift
+//  SCBTestAssignment
+//
+//  Created by Nuan on 29/12/2562 BE.
+//  Copyright © 2562 Hathairat. All rights reserved.
+//
+
+import UIKit
+class MyTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var mobiePhonImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
