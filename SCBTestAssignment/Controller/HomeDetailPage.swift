88d8586956f068db9be69b8d63cea1be35d2548e
Hathairat <Hathairat.ssk@gmail.com>
2019-12-31 00:52:46 +0700

    back up

---
+++ SCBTestAssignment/Controller/HomeDetailPage.swift
@@ -0,0 +1,14 @@
+//
+//  HomeDetailPage.swift
+//  SCBTestAssignment
+//
+//  Created by Nuan on 30/12/2562 BE.
+//  Copyright © 2562 Hathairat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class HomeDetailPage: UIViewController {
+    
+    @IBOutlet weak var showCollectionView: UICollectionView!
+}
---
+++ SCBTestAssignment/View/MyHome.swift
@@ -0,0 +1,13 @@
+//
+//  MyHome.swift
+//  SCBTestAssignment
+//
+//  Created by Nuan on 30/12/2562 BE.
+//  Copyright © 2562 Hathairat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyHome {
+    
+}
